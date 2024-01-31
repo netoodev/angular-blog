30094e59085abf66806f3ddd0947aeb6199554aa
Neto <netoodeveloper@gmail.com>
2024-01-31 15:01:13 -0300

    Desacoplando os componentes da parte visual

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
   styleUrl: './big-card.component.css'
 })
-export class BigCardComponent {
+export class BigCardComponent implements OnInit {
 
+  @Input()
+  photoCover:string = 'https://secureservercdn.net/166.62.110.60/h65.3a1.myftpupload.com/wp-content/uploads/2021/09/variable-placeholder-product-31.jpg?time=1644500349';
+
+  @Input()
+  cardDate:string = 'DD de Mês, AAAA'
+
+  @Input()
+  cardTitle:string = 'Título';
+
+  @Input()
+  cardDescription:string = 'Descrição';
+
+  @Input()
+  cardSrc:string = '';
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
   styleUrl: './small-card.component.css'
 })
-export class SmallCardComponent {
+export class SmallCardComponent implements OnInit {
+
+  @Input()
+  photoCover:string = 'https://secureservercdn.net/166.62.110.60/h65.3a1.myftpupload.com/wp-content/uploads/2021/09/variable-placeholder-product-31.jpg?time=1644500349';
+
+  @Input()
+  cardDate:string = 'DD de Mês, AAAA'
+
+  @Input()
+  cardTitle:string = 'Título';
+
+  @Input()
+  cardSrc:string = '';
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
 
 }
