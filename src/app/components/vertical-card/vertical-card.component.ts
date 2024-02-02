daed099d64df8d9ab85d99c6a7d5ea8170b56ff5
Neto <netoodeveloper@gmail.com>
2024-02-01 22:38:33 -0300

    Criação de segunda seção na home

---
+++ src/app/components/vertical-card/vertical-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vertical-card',
+  templateUrl: './vertical-card.component.html',
+  styleUrl: './vertical-card.component.css'
+})
+export class VerticalCardComponent {
+
+}
