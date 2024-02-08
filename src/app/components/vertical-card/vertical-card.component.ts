60ad57bacf9e7da4f465deb2ce5bd214bb742f3c
Neto <netoodeveloper@gmail.com>
2024-02-08 10:09:05 -0300

    Seção Universo Literário

--- src/app/components/vertical-card/vertical-card.component.ts
+++ src/app/components/vertical-card/vertical-card.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-vertical-card',
   templateUrl: './vertical-card.component.html',
   styleUrl: './vertical-card.component.css'
 })
-export class VerticalCardComponent {
+export class VerticalCardComponent implements OnInit {
+
+  @Input()
+  photoCover:string = 'https://secureservercdn.net/166.62.110.60/h65.3a1.myftpupload.com/wp-content/uploads/2021/09/variable-placeholder-product-31.jpg?time=1644500349';
+
+  @Input()
+  photoAlt:string = 'Imagem';
+
+  @Input()
+  cardDate:string = 'DD de Mês, AAAA'
+
+  @Input()
+  cardTitle:string = 'Título';
+
+  @Input()
+  id:string = '0';
+
+  ngOnInit(): void {
+  }
 
 }
